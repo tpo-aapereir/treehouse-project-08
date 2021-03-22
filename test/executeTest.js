9fd9088d42915dc3de195901706b1a7ff03a698c
tpo-aapereir <aapereir@travelers.com>
2021-03-22 15:20:55 -0400

    updated CSS and test code

--- test/executeTest.js
+++ test/executeTest.js
@@ -1,8 +1,8 @@
-const test = require('th_unit08_test_suite');
-var path = require('path');
+const test = require('th_unit08_test_suite')
+var path = require('path')
 
 // we define a global app root variable so we can determine the root of the application
-global.appRoot = path.resolve(__dirname + "\\..\\");
+global.appRoot = path.resolve(__dirname + "\\..\\")
 
 // we manually execute the tests in the included package
-test.executeMeetsTest();
+test.executeMeetsTest()
