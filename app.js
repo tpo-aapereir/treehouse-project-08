d0f14bc036447706ad00b520eebbcbeabf7aa192
tpo-aapereir <aapereir@travelers.com>
2021-03-23 15:46:32 -0400

    updated code for errors

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/', indexRouter);
   }
 })()
 
-// catch 404 and forward to error handler
+// catch 404 from an undefined URL and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
 })
